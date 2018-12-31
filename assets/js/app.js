5db255e49748f0f2853cc2b13e8d82003a80c182
Junbum Lee <jun@beomi.net>
2018-12-31 23:00:39 +0900

    Not using js on blog post

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,8 @@ var Tabs = (function() {
 
   return {
     settings: {
-      tabs: document.getElementsByClassName('tabs__item'),
-      tab: document.getElementsByClassName('tab')
+      tabs: document.getElementsByClassName("tabs__item"),
+      tab: document.getElementsByClassName("tab")
     },
 
     init: function() {
@@ -16,37 +16,36 @@ var Tabs = (function() {
     display: function() {
       if (s.tab.length) {
         [].forEach.call(s.tab, function(tab, idx) {
-          if (idx > 0) tab.style.display = 'none';          
+          if (idx > 0) tab.style.display = "none";
         });
-        s.tab[0].classList.add('active');
-        s.tabs[0].classList.add('active');
+        s.tab[0].classList.add("active");
+        s.tabs[0].classList.add("active");
       }
     },
 
     click: function() {
       if (s.tabs.length) {
         var currentIdx = 0,
-            prevIdx = currentIdx;
+          prevIdx = currentIdx;
 
         [].forEach.call(s.tabs, function(tab, idx) {
-          tab.addEventListener('click', function() {
+          tab.addEventListener("click", function() {
             prevIdx = currentIdx;
             currentIdx = idx;
 
             if (prevIdx !== currentIdx) {
-              s.tab[prevIdx].style.display = 'none';
-              s.tab[prevIdx].classList.remove('active');
-              s.tabs[prevIdx].classList.remove('active');
-              s.tab[currentIdx].style.display = 'block';
-              s.tab[currentIdx].classList.add('active');
-              s.tabs[currentIdx].classList.add('active');
+              s.tab[prevIdx].style.display = "none";
+              s.tab[prevIdx].classList.remove("active");
+              s.tabs[prevIdx].classList.remove("active");
+              s.tab[currentIdx].style.display = "block";
+              s.tab[currentIdx].classList.add("active");
+              s.tabs[currentIdx].classList.add("active");
             }
           });
         });
       }
     }
-
-  }
+  };
 })();
 
 var Preview = (function() {
@@ -54,8 +53,8 @@ var Preview = (function() {
 
   return {
     settings: {
-      img: document.getElementsByClassName('preview__img'),
-      post: document.getElementsByClassName('preview')
+      img: document.getElementsByClassName("preview__img"),
+      post: document.getElementsByClassName("preview")
     },
 
     init: function() {
@@ -67,7 +66,7 @@ var Preview = (function() {
     display: function() {
       if (s.img.length) {
         [].forEach.call(s.img, function(img, idx) {
-          if (idx > 0) img.style.display = 'none';
+          if (idx > 0) img.style.display = "none";
         });
       }
     },
@@ -75,31 +74,31 @@ var Preview = (function() {
     mouseenter: function() {
       if (s.post.length) {
         var currentIdx = 0,
-            prevIdx = currentIdx;
+          prevIdx = currentIdx;
 
         [].forEach.call(s.post, function(preview, idx) {
-          preview.addEventListener('mouseenter', function() {
+          preview.addEventListener("mouseenter", function() {
             prevIdx = currentIdx;
             currentIdx = idx;
 
             if (prevIdx !== currentIdx) {
-              s.img[prevIdx].style.display = 'none';
-              s.img[currentIdx].style.display = 'block';
+              s.img[prevIdx].style.display = "none";
+              s.img[currentIdx].style.display = "block";
             }
           });
         });
       }
     }
-  }
+  };
 })();
 
 var wow = new WOW({
-  animateClass: 'fade-in'
+  animateClass: "fade-in"
 });
 
-// var rellax = new Rellax('.rellax');
+var rellax = new Rellax(".rellax");
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   Tabs.init();
   Preview.init();
   wow.init();
